9705d71aa6d076429fee05cc4932c0012d965bbe
Soha Refaat <144360872+soha-Refaat@users.noreply.github.com>
2023-12-01 05:47:34 -0800

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,18 @@
+let output=document.getElementById("clc");
+function display(num){
+    output.value +=num;
+}
+function Clear(){
+    output.value ="";
+}
+function Delete(){
+    output.value=output.value.slice(0,-1);
+}
+function calc(){
+    try{
+        output.value=eval(output.value);
+    }
+    catch(err){
+        output.value="Error..."
+    }
+}
